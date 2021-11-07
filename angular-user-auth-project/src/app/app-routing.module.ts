43cd5f9e39e33074e27a5602c7f9ed15123939e8
melike.eryilmaz <melikeeryilmaz1@gmail.com>
2021-11-07 15:29:39 +0300

    components and modules created

--- angular-user-auth-project/src/app/app-routing.module.ts
+++ angular-user-auth-project/src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './customer/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'admin',
+
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./admin/admin.module').then((m) => m.AdminModule),
+      },
+    ],
+  },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: '' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
